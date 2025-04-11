09dc4d95c1925959484ee644cbcbcfe2f5da7605
kagepota <nelchrimson@gmail.com>
2025-04-11 21:08:22 +0800

    re

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+ $db_server = "Localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "tcinv";
+    $conn = "";
+
+    if($conn){
+        echo "Connected";
+    }else{
+        echo "Not Connected";
+    }
+?> 
